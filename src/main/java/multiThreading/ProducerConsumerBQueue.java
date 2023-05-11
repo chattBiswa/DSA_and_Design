0d10c3d3487d4cec5c1dca18d4452e1ca4dc30f4
Administrator <chatterjeetechbiswajeet.11@gmail.com>
2023-05-11 21:53:17 +0530

    retrieve deleted files

---
+++ src/main/java/multiThreading/ProducerConsumerBQueue.java
@@ -0,0 +1,56 @@
+package multiThreading;
+
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ProducerConsumerBQueue {
+    public ProducerConsumerBQueue() {
+    }
+
+    public static void main(String[] args) {
+        BlockingQueue sharedQueue = new LinkedBlockingQueue();
+        Thread proT = new Thread(new Producer(sharedQueue));
+        Thread conT = new Thread(new Consumer(sharedQueue));
+        proT.start();
+        conT.start();
+    }
+}
+
+class Producer implements Runnable {
+    private final BlockingQueue sharedQueue;
+
+    public Producer(BlockingQueue sharedQueue) {
+        this.sharedQueue = sharedQueue;
+    }
+
+    public void run() {
+        for(int i = 1; i < 10; ++i) {
+            try {
+                System.out.println("Produce = " + i);
+                this.sharedQueue.put(i);
+            } catch (InterruptedException var3) {
+                throw new RuntimeException(var3);
+            }
+        }
+
+    }
+}
+
+class Consumer implements Runnable {
+    private final BlockingQueue sharedQueue;
+
+    public Consumer(BlockingQueue sharedQueue) {
+        this.sharedQueue = sharedQueue;
+    }
+
+    public void run() {
+        while(true) {
+            try {
+                System.out.println("Consume = " + this.sharedQueue.take());
+            } catch (InterruptedException var2) {
+                throw new RuntimeException(var2);
+            }
+        }
+    }
+}
---
+++ src/main/java/multiThreading/SequenceExec.java
@@ -0,0 +1,41 @@
+package multiThreading;
+
+public class SequenceExec {
+    public static void main(String[] args) {
+        ParallelTask p1 = new ParallelTask();
+        ParallelTask p2 = new ParallelTask();
+        ParallelTask p3 = new ParallelTask();
+        Thread t1 = new Thread(p1, "Thread-1");
+        Thread t2 = new Thread(p2, "Thread-2");
+        Thread t3 = new Thread(p3, "Thread-3");
+        p2.setPredecessor(t1);
+        p3.setPredecessor(t2);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    private static class ParallelTask implements Runnable {
+        Thread predecessor;
+
+        private ParallelTask() {
+        }
+
+        void setPredecessor(Thread t) {
+            this.predecessor = t;
+        }
+
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + " started");
+            if (this.predecessor != null) {
+                try {
+                    this.predecessor.join();
+                } catch (InterruptedException var2) {
+                    var2.printStackTrace();
+                }
+            }
+
+            System.out.println(Thread.currentThread().getName() + " finished");
+        }
+    }
+}
